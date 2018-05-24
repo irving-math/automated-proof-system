f74e14279872b7e5d0f890b333e011995dec5479
Irving Flores <ij.flores@icloud.com>
2018-05-24 03:22:45 -0500

    Arreglo final final

--- Haskell/AutomaticProof.hs
+++ Haskell/AutomaticProof.hs
@@ -8,14 +8,11 @@ import Variable
 import Language.Haskell.TH
 
 
-$(defineVariables ["X", "Y", "Z"])
+$(defineVariables ["X", "Y", "Z", "X1", "X2", "X3", "Y1", "Y2", "Y3", "Z1", "Z2", "Z3"])
 
 -- Ideal generico
-ideal :: [Polynomial Rational (X :<: Y) Lex]
-ideal = [x ^ 10 + x ^ 9 * y ^ 2, y ^ 8 - x ^ 2 * y ^ 7]
-
-basis :: [Polynomial Rational (X :<: Y) Lex] -> [Polynomial Rational (X :<: Y) Lex]
-basis ideal' = groebner ideal'
+ideal_generic :: [Polynomial Rational (X :<: Y) Lex]
+ideal_generic = [x ^ 10 + x ^ 9 * y ^ 2, y ^ 8 - x ^ 2 * y ^ 7]
 
 isIn ::(Eq r, Fractional r, Ord v, Show v, Ord (Monomial v Lex))
      =>  Polynomial r v Lex -> [Polynomial r v Lex] -> Bool
--- Haskell/Constants.hs
+++ Haskell/Constants.hs
@@ -7,3 +7,10 @@ import Variable
 --Primer problema
 ideal_punto_2_2 :: [Polynomial Rational (X :<: Y) Lex]
 ideal_punto_2_2 = [6*x-6*y,x-2]
+
+--Paralelas en un cuadrado
+ideal_paralelas :: [Polynomial Rational (X1 :<: X2 :<: X3 :<: Y1 :<: Y2 :<: Y3 :<: Z1 :<: Z2 :<: Z3) Lex]
+ideal_paralelas = [y1*y3+x1*x3, y3*y2-y3^2+x2*x3-x3^2, z2*z3*x3*y3-z2*x3-z3*y3+1]
+
+poli_paralelas :: Polynomial Rational (X1 :<: X2 :<: X3 :<: Y1 :<: Y2 :<: Y3 :<: Z1 :<: Z2 :<: Z3) Lex
+poli_paralelas = y1*x2-y1*x3-x1*y2+x1*y3
